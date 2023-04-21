bfd0dc842f692e3179e4f436c19509e64fa56b4a
Yoanna16 <yoannaborisova@gmail.com>
2023-04-21 11:13:31 +0200

    add logic to auth component

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,11 +2,25 @@ import React, { useState } from 'react'
 
 const Auth = () => {
   const [isLogIn, setIsLogIn] = useState(true);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
 
   const viewLogin = (status) => {
     setError(null)
     setIsLogIn(status)
+  }
+
+  const handleSubmit = async (e, endpoint) => {
+    e.preventDefault()
+
+    if(!isLogIn && password !== confirmPassword) {
+      setError('Make sure passwords match!')
+      return
+    }
+
+    await fetch(`${process.env.REACT_APP_SERVERURL}/${endpoint}`)
   }
 
   return (
@@ -17,7 +31,7 @@ const Auth = () => {
           <input type='email' placeholder='email'/>
           <input type='password' placeholder='password'/>
           {!isLogIn && <input type='password' placeholder='confirm password'/>}
-          <input type='submit' className='creeate'/>
+          <input type='submit' className='creeate' onClick={(e)=> handleSubmit(e, isLogIn ? 'login' : 'signup')}/>
           {error && <p>{error}</p>}
         </form>
 
