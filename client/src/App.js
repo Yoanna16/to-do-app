17fbb621c2d5e74f8b94344b65c3e2cdc1e35ef4
Yoanna16 <yoannaborisova@gmail.com>
2023-04-21 15:42:45 +0200

    fix the progress bar

--- client/src/App.js
+++ client/src/App.js
@@ -36,10 +36,10 @@ const App = () =>  {
       { authToken &&
       <>
       <ListHeader listName={' Holiday tick list'} getData={getData}/>
+      <p className="user-email">Welcome back {userEmail}</p>
       {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
       </>
       }
-
     </div>
   );
 }
--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import Modal from './Modal'
+import { useCookies } from 'react-cookie';
 
 const ListHeader = ({ listName, getData }) => {
+    const [cookies, setCookie, removeCookie] = useCookies(null);
 
     const [showModal, setShowModal] = useState(false);
 
     const signOut = () => {
         console.log('sign out')
+        removeCookie('Email')
+        removeCookie('AuthToken')
+
+        window.location.reload();
     }
 
     return (
